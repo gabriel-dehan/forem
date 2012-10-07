0cbb08e3f8400137bf39ec69d561f9c24e3526c5
gabriel-dehan <dehan.gabriel@gmail.com>
2012-10-08 01:37:25 +0200

    Corrected column type in migration

--- db/migrate/20121007222114_create_forem_configurations.rb
+++ db/migrate/20121007222114_create_forem_configurations.rb
@@ -1,7 +1,7 @@
 class CreateForemConfigurations < ActiveRecord::Migration
   def change
     create_table :forem_configurations do |t|
-      t.bool :post_approval
+      t.boolean :post_approval
 
       t.timestamps
     end
