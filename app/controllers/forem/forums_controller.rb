1389cabe3bb5004635a6502d7dd6bb87167679a3
gabriel-dehan <dehan.gabriel@gmail.com>
2012-10-08 01:52:31 +0200

    Fixed configuration initialization

--- app/controllers/forem/forums_controller.rb
+++ app/controllers/forem/forums_controller.rb
@@ -4,7 +4,7 @@ module Forem
     helper 'forem/topics'
 
     def index
-      Forem::Configuration.create(post_approval: true) unless Forem::Configuration.find.empty?
+      Forem::Configuration.create(post_approval: true) unless Forem::Configuration.all.empty?
       @categories = Forem::Category.all
     end
 
