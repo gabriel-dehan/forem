54ce423d8256da9701d37b4dcf71868d87b0106f
gabriel-dehan <dehan.gabriel@gmail.com>
2012-10-08 02:36:39 +0200

    Bug in configuration index

--- app/controllers/forem/admin/configuration_controller.rb
+++ app/controllers/forem/admin/configuration_controller.rb
@@ -1,18 +1,16 @@
 module Forem
-  module Admin
-    class ConfigurationController < ApplicationController
-      def index
-        @configuration = Forem::Configuration.first
-      end
+  class Admin::ConfigurationController < BaseController
+    def index
+      @configuration = Forem::Configuration.first
+    end
 
-      def update
-        if @conf.update_attributes(params[:configuration])
-          flash[:notice] = t("forem.admin.configuration.updated")
-          redirect_to admin_configuration_path
-        else
-          flash.now.alert = t("forem.admin.configuration.not_updated")
-          render :action => "index"
-        end
+    def update
+      if @conf.update_attributes(params[:configuration])
+        flash[:notice] = t("forem.admin.configuration.updated")
+        redirect_to admin_configuration_path
+      else
+        flash.now.alert = t("forem.admin.configuration.not_updated")
+        render :action => "index"
       end
     end
   end
