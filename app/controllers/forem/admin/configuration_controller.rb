a9d3b249a0bae480200d66081983d5130df9bb0c
gabriel-dehan <dehan.gabriel@gmail.com>
2012-10-08 03:11:19 +0200

    Configuration update fix

--- app/controllers/forem/admin/configuration_controller.rb
+++ app/controllers/forem/admin/configuration_controller.rb
@@ -1,20 +1,19 @@
 module Forem
-  class Admin::ConfigurationController < ApplicationController
-    def index
-      @configuration = Forem::Configuration.first
-    end
+  module Admin
+    class ConfigurationController < BaseController
+      def index
+        @configuration = Forem::Configuration.first
+      end
 
-    def update
-      p @configuration
-      p Forem::Configuration.first
-      p Configuration.first
-      @configuration ||= Forem::Configuration.first
-      if @configuration.update_attributes(params[:configuration])
-        flash[:notice] = t("forem.admin.configuration.updated")
-        redirect_to admin_configuration_path
-      else
-        flash.now.alert = t("forem.admin.configuration.not_updated")
-        render :action => "index"
+      def update
+        @configuration ||= Forem::Configuration.first
+        if @configuration.update_attributes(params[:configuration])
+          flash[:notice] = t("forem.admin.configuration.updated")
+          redirect_to admin_configuration_path
+        else
+          flash.now.alert = t("forem.admin.configuration.not_updated")
+          render :action => "index"
+        end
       end
     end
   end
